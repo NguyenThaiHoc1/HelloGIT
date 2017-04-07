72221822e6676f340b1dab7a518a583b2ece5cf1
NguyenThaiHoc1 <thaihocmap123@gmail.com>
2017-04-07 09:51:32 +0700

    Setting Server and Routing1

--- server.js
+++ server.js
@@ -1,5 +1,45 @@
 var express = require("express");
+var bodyparser = require("body-parser");
+var pg = require("pg");
 var app = express();
+
+var fs = require("fs");
+// setting a ServerFile Project
+  // . setting public include: css, font, js
+app.use(express.static(__dirname + "/public"));
+  // . setting Template engine
+app.set("view engine", "ejs");
+app.set("views", "./views");
+  // . setting body-parser
+var urlencodeParser = bodyparser.urlencoded({extended:false});
+  // . setting PostGres
+  // comming soon
+
+// Home Page
+app.get("/", function (req, res) {
+
+});
+
+// About Page
+app.get("/About", function (req, res) {
+
+});
+
+// Blogs Page
+app.get("/Blogs", function (req, res) {
+
+});
+
+// Albums Page
+app.get("/Albums", function (req, res) {
+
+});
+  // Sub-Album
+
+// Register Page
+app.get("/register", function (req, res) {
+  res.render("register");
+});
 
 app.listen(8080, function() {
  console.log("Server is connecting in Port: 8080");
